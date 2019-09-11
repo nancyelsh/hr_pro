22049fd639c431c49d5267baec5b1170f32505c7
Nour El-Sharkawi <Nour@Nours-MacBook-Pro-703.local>
2019-09-11 21:41:18 +0300

    File's been modified again

--- madlibs.py
+++ madlibs.py
@@ -7,4 +7,7 @@ name = raw_input("Enter a name: ")
 scream = raw_input("Enter any sentence: ")
 action = raw_input("Enter a verb: ")
 
-print 'It was %s o\'clock when I heard a knock at the door. I opened the door and there was a box full of %s with a note saying "From Mr. %s." Just as I closed the door I heard a scream "%s." I froze in place and all I could do was %s.' % (time, items, name.title(), scream.upper(), action)
+print """It was %s o\'clock when I heard a knock at the door.
+I opened the door and there was a box full of %s with a note saying "From Mr. %s."
+Just as I closed the door I heard a scream "%s."
+I froze in place and all I could do was %s.' % (time, items, name.title(), scream.upper(), action)
