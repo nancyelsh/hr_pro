8c8cb65f43b88463581718a85bd990039b08275a
Nour El-Sharkawi <Nour@Nours-MacBook-Pro-703.local>
2019-09-11 21:49:29 +0300

    Mistake's been corrected

--- madlibs.py
+++ madlibs.py
@@ -10,4 +10,4 @@ action = raw_input("Enter a verb: ")
 print """It was %s o\'clock when I heard a knock at the door.
 I opened the door and there was a box full of %s with a note saying "From Mr. %s."
 Just as I closed the door I heard a scream "%s."
-I froze in place and all I could do was %s.' % (time, items, name.title(), scream.upper(), action)
+I froze in place and all I could do was %s.""" % (time, items, name.title(), scream.upper(), action)
