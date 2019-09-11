f88a29d0180c91ebfcd1cc524e4aea8424fd8a0d
Nour El-Sharkawi <Nour@Nours-MacBook-Pro-703.local>
2019-09-11 21:27:19 +0300

    File's been finalized

---
+++ madlibs.py
@@ -0,0 +1,10 @@
+print """Mad libs where libs get mad.
+Start below:"""
+print ""
+time = raw_input("Enter a number from 1 to 12: ")
+items = raw_input("Enter a noun (plural): ")
+name = raw_input("Enter a name: ")
+scream = raw_input("Enter any sentence: ")
+action = raw_input("Enter a verb: ")
+
+print 'It was %s o\'clock when I heard a knock at the door. I opened the door and there was a box full of %s with a note saying "From Mr. %s." Just as I closed the door I heard a scream "%s." I froze in place and all I could do was %s.' % (time, items, name.title(), scream.upper(), action)
