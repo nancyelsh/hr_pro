90c25723ae7a9478e982a968b8057f637deff503
Nour El-Sharkawi <Nour@Nours-MacBook-Pro-703.local>
2019-09-13 13:25:29 +0300

    File's been modified

--- calculator.py
+++ calculator.py
@@ -2,20 +2,11 @@ first_no = input("Enter the first number: ")
 second_no = input("Enter the second number: ")
 operation = input("Choose the operation (+, -, *, /): ")
 
-#if type("first_no") != int or float:
-#  print("First Number is invalid")
-#if type("second_no") != int or float:
-#  print("Second Number is invalid")
-
 try:
    val1 = int(first_no)
-except ValueError:
-   print("That's not an int!")
-
-try:
    val2 = int(second_no)
 except ValueError:
-   print("That's not an int!")
+   print("Numbers were invalid")
 
 if operation == '+':
   print("The answer is {}".format((int(first_no) + int(second_no))))
@@ -26,4 +17,4 @@ elif operation == '*':
 elif operation == '/':
   print("The answer is {}".format((int(first_no) / int(second_no))))
 else:
-  print("The operation is invalid")
+  print("Operation is invalid")
