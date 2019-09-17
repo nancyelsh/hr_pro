6d1edd9445bf494143f810ac0e35640421a9e2ca
Nour El-Sharkawi <Nour@Nours-MacBook-Pro-703.local>
2019-09-17 18:07:52 +0300

    Finally

--- recruitment_program.py
+++ recruitment_program.py
@@ -1,15 +1,10 @@
 skills = ['Python', 'C++', 'Javascript', 'Juggling', 'Running', 'Eating']
 cv = {}
-
 print("Welcome to the special recruitment program, please answer the following questions: ")
-cv['name'] = input("What's your name?")
-name = cv['name']
-cv['age'] = input("How old are you?")
-age = cv['age']
-cv['experience'] = input("How many years of experience do you have?")
-experience = cv['experience']
+cv['name'] = input("What's your name? ")
+cv['age'] = input("How old are you? ")
+cv['experience'] = input("How many years of experience do you have? ")
 cv['skills'] = []
-
 print("""Skills:
 1- Python
 2- C++
@@ -17,38 +12,9 @@ print("""Skills:
 4- Juggling
 5- Running
 6- Eating""")
-
-cv['skills'][0].append(input("Choose a skill from above by entering its number: ")
-#cv['skills']append(input("Choose another skill from above by entering its number: ")
-
-"""if age > 25:
-  if age < 40:
-    if experience > 3:
-      if 'Pythons' in skills:
-        print("You have been accepted, name")
+cv['skills'].append(input("Choose a skill from above by entering its number: "))
+cv['skills'].append(input("Choose another skill from above by entering its number: "))
+if int(cv['age']) > 25 and int(cv['age']) < 40 and int(cv['experience']) > 3 and 'Python' in skills:
+    print("You have been accepted, {}!".format(cv['name']))
 else:
-  print("We're sorry, name")"""
-
-while True:
-  try:
-   cv['age'] > 25
-  except ValueError:
-   break
-
-  try:
-   cv['age'] < 4
-  except ValueError:
-   break
-
-  try:
-   cv['experience'] > 3
-  except ValueError:
-   break
-
-  try:
-   'Pythons' in cv['skills']
-   print("You have been accepted, cv['name']")
-  except ValueError:
-   break
-
-print("We're sorry, name")
+    print("We're sorry, {}.".format(cv['name']))
